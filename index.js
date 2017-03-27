16a4daa7dd063e1bce2d9b0de9279187457f8477
Soichiro-Yoshimura <yoshimura@soichiro.org>
2017-03-27 17:34:26 +0900

    新規Q&Aのみメール対応

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const client = require('cheerio-httpcli');
 const request = require('request');
 const fs = require('fs');
+const nodemailer = require('nodemailer');
 
 // 設定読み込み
 const configJson = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
@@ -61,7 +62,7 @@ const pQuestions = pQuestionList.then((result) => {
 });
 
 // 取得したQ&Aを処理
-pQuestions.then((questions) => {
+let pFinished = pQuestions.then((questions) => {
   console.log('----取得したQ&A----');
   console.log(questions);
 
@@ -72,7 +73,9 @@ pQuestions.then((questions) => {
     // 取得したものの先頭から処理して、1分前のものにあれば投稿
     for (let q of questions) {
       if (!questionsLinkSet.has(q.link)) {
-        let message = '【新規Q&A】: [' + q.tags + '] by ' + q.name + '\n' +
+        // Slackに送信
+        let title = '【新規Q&A】: [' + q.tags + '] by ' + q.name;
+        let message = title + '\n' +
                       q.question + '\nhttp://www.nnn.ed.nico' + q.link;
         let headers = {
           'Content-Type': 'application/json'
@@ -98,6 +101,25 @@ pQuestions.then((questions) => {
             console.log(message);
           }
         });
+
+        // Mail 送信
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport(configJson.mailSetting);
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: '"N予備Q&A" <nyobi_qa@nnn.ed.jp>', // sender address
+            to: 'nyobi_qa@nnn.ed.jp', // list of receivers
+            subject: title, // Subject line
+            text: message // plain text body
+        };
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message %s sent: %s', info.messageId, info.response);
+        });
+
       } else {
         break;
       }
@@ -109,4 +131,11 @@ pQuestions.then((questions) => {
       console.log('ファイルに取得した取得したQ&Aを保存しました。');
     });
   }
+});
+
+pFinished.then(() => {
+  console.log('Finished.');
+}).catch((e) => {
+  console.log('Error occured:');
+  console.log(e);
 });
